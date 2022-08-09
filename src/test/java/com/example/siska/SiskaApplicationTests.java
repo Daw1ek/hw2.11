ffb6a728d23be99a79d8054d103710c372bb99bb
xpork <xporkshopx@gmail.com>
2022-08-09 17:07:39 +0300

    Initial commit

---
+++ src/test/java/com/example/siska/SiskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
